a600c9f2d3c25701c6bb9a52420b82b4c9c27ff1
= <=>
2025-11-17 13:31:41 +0100

    aggionamento

---
+++ src/SynchronizedBlockExample.java
@@ -0,0 +1,14 @@
+public class SynchronizedBlockExample {
+    private final Object lock = new Object();
+    private int count = 0;
+
+    public void increment() {
+        synchronized (lock) {
+            count++;
+        }
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
